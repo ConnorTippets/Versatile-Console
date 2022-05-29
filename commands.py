20a682b313ba4d09629eb46e6d8df35f2101b2e2
Con <50535379+ConnorTippets@users.noreply.github.com>
2022-05-28 20:10:36 -0500

    1.0

---
+++ commands.py
@@ -0,0 +1,32 @@
+def command_getparams(command_name, src):
+    src_fixed = src[len(command_name)+1:]
+    src_fixed = src_fixed.split(" ")
+    conjoined1 = []
+    conjoined2 = []
+    for idx, param in enumerate(src_fixed):
+        if param.startswith('"'):
+            for idxx in range(idx+1, len(src_fixed)):
+                conjoined = [param+" "]
+                if src_fixed[idxx].endswith('"'):
+                    conjoined[0] = conjoined[0]+src_fixed[idxx]
+                    conjoined1.append(conjoined)
+                    break
+                else:
+                    conjoined[0] = conjoined[0]+src_fixed[idxx]+" "
+    for idx, param in enumerate(src_fixed):
+        if param.startswith("'"):
+            for idxx in range(idx+1, len(src_fixed)):
+                conjoined = [param+" "]
+                if src_fixed[idxx].endswith("'"):
+                    conjoined[0] = conjoined[0]+src_fixed[idxx]
+                    conjoined2.append(conjoined)
+                    break
+                else:
+                    conjoined[0] = conjoined[0]+src_fixed[idxx]+" "
+    return src_fixed, conjoined1, conjoined2
+
+def command_help():
+    help_txt = open("help_text.txt", "r")
+    commands = help_txt.read()
+    help_txt.close()
+    return commands
---
+++ console.py
@@ -0,0 +1,19 @@
+import commands
+
+def console(command_list):
+    command = input("")
+    command_name = command.split(" ")[0]
+    if command_name in command_list:
+        if command_name == "help":
+            print(commands.command_help())
+        return console(command_list)
+    else:
+        print("Syntax Error: Not a command")
+        return console(command_list)
+
+print("Versatile Console 1.0 Initialized")
+print("Type help for a list of commands")
+commands_file = open("commands.txt", "r")
+command_list = commands_file.read().split("\n")
+commands_file.close()
+console(command_list)
